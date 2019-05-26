b89b47a0be3119eb76f463c4524a8f8c93323d09
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-05-26 14:45:21 +0000

    Add a good message describing what you actually changed since your last commit in hereexit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,15 +1,12 @@
 
-class Course < ApplicationRecord 
+class Course < ApplicationRecord
   mount_uploader :image, ImageUploader
-  serialize :image, JSON # If you use SQLite, add this line.
-  belongs_to :user
 
+  belongs_to :user
   has_many :sections
+  has_many :enrollments
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-
-
-
 end
